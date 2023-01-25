2f5f417a221a4b112703e7debfb73877a50b6cbc
jay123q <106704608+jay123q@users.noreply.github.com>
2023-01-25 08:58:39 -0600

    First edits to code setting up boilerplate

---
+++ hw1/hw1.py
@@ -0,0 +1,19 @@
+import pandas as p
+
+def read_data(name):
+    return p.read_csv(name)
+
+def get_df_shape(pandaRead):
+    
+
+def main():
+    fileName = "Smarket"
+    read_data(fileName)
+
+
+
+
+
+
+if ( __name__ == "__main__"):
+    main()
